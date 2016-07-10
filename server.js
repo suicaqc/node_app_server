6cb75a633fde9fb926166c24d6ee387ac13e833e
john shu <b2blosangeles@gmail.com>
2016-07-10 14:40:40 +0000

    server.js edited online with Bitbucket

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ var express = require('express'),
 app			= express(),
 expireTime	= 604800000,
 Nedb 		= require('nedb'),
-db 			= new Nedb({ filename: 'db/cache.db', autoload: true }),
+
+db 			= {
+				cache 	: new Nedb({ filename: 'db/cache.db', autoload: true }),
+				auth	: new Nedb({ filename: 'db/auth.db', autoload: true })
+			},
+
 port 		= 8880;
 
+
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/test.js');
+      db.auth.find({ user: 67 }, function (err, docs) {
+        	res.send(docs);
+        // docs is an array containing Earth and Mars
+      }); 
+
+ // res.sendFile(__dirname + '/test.js');
 });
 
 app.use(express.static(__dirname,{ maxAge: expireTime}));
