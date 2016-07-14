b53cc2580b630c5d183c3c282bcd1b1d71cf3db0
b2blosangeles <b2blosangeles@gmail.com>
2016-07-14 08:34:03 -0700

    test

--- qalet_cache.js
+++ qalet_cache.js
@@ -1,8 +1,9 @@
 var 
 express = require('express'),  
-crowdProcess = require('crowdProcess'), Nedb = require('nedb'),
+crowdProcess = require('crowdProcess'), 
+Nedb = require('nedb'), 
+bodyParser = require('body-parser'),
 request = require('request'),
-
 app			= express(),
 expireTime	= 604800000,
 
@@ -11,8 +12,74 @@ db 			= {
 				auth	: new Nedb({ filename: 'db/auth.db', autoload: true })
 			},
 
-port 		= 80;
+port 		= 8880;
 
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.post(/cache(|[0-9]+)\/(\S+)$/i, function(req, res) {
+	var CP = new crowdProcess();
+	var _f = {};
+	var _cachetime = 1000 * ((req.params[0])?req.params[0]:3600);
+
+	_f['S1'] = function(cbk) {
+		db.cache.find({ source: req.params[1], postdata:JSON.stringify(req.body) }, function (err, docs) {
+	    	if ((docs[0]) && (new Date() - docs[0].tm < _cachetime)) {
+	    		cbk(docs[0]);
+	    		CP.exit = true;
+	    	} else {	    		
+	    		db.cache.remove({ source: req.params[1], postdata:JSON.stringify(req.body) }, function (err, docs) {
+	    			cbk(false);
+	    		});	
+	    	}
+	    	
+	      });
+     };	
+
+	_f['S2'] = function(cbk) {
+	    var options = {
+	        url: req.params[1],
+	        method:  'POST',
+			data: req.body,
+	        encoding: null
+	    }
+
+	    request(options ,function(error, response, body) {
+	    	if (error) {
+	    		res.send(error.toString());
+	    		cbk(false);
+	    	} else {
+		    	var rec = { 
+					source: req.params[1], 
+					postdata:JSON.stringify(req.body),
+					cache: new Buffer(body).toString('base64'), 
+					tm: new Date(), 
+					content_type:response.headers['content-type']};
+				db.cache.insert(rec, function (err) {
+					cbk(rec);
+				  });
+	    	}
+	    });
+     };	
+
+	CP.serial(
+		_f,
+		function(data) {
+	    	var rec = (data.results.S1)?data.results.S1:data.results.S2;
+	    	if (rec !== false) {
+		    	res.writeHead(200, {'Content-Type': rec.content_type});
+		    	res.write(new Buffer(rec.cache, 'base64'));
+		    	res.end();	    		
+	    	}
+
+		},
+		3000
+	);
+});
 
 
 app.get(/cache(|[0-9]+)\/(\S+)$/i, function (req, res) {
