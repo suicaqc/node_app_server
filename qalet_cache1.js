c86ddbc7e314ef0c437941ecf0b2a9f8b7761536
b2blosangeles <b2blosangeles@gmail.com>
2016-07-14 08:27:36 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -21,9 +21,64 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.post('/', function(req, res) {
-   res.send(req.body);
-    // ...
+app.post(/cache(|[0-9]+)\/(\S+)$/i, function(req, res) {
+	var CP = new crowdProcess();
+	var _f = {};
+	var _cachetime = 1000 * ((req.params[0])?req.params[0]:3600);
+
+	_f['S1'] = function(cbk) {
+		db.cache.find({ source: req.params[1], postdata:JSON.stringify(req.body) }, function (err, docs) {
+	    	if ((docs[0]) && (new Date() - docs[0].tm < _cachetime)) {
+	    		cbk(docs[0]);
+	    		CP.exit = true;
+	    	} else {	    		
+	    		db.cache.remove({ source: req.params[1], postdata:JSON.stringify(req.body) }, function (err, docs) {
+	    			cbk(false);
+	    		});	
+	    	}
+	    	
+	      });
+     };	
+
+	_f['S2'] = function(cbk) {
+	    var options = {
+	        url: req.params[1],
+	        method:  'POST',
+			data: req.body,
+	        encoding: null
+	    }
+
+	    request(options ,function(error, response, body) {
+	    	if (error) {
+	    		res.send(error.toString());
+	    		cbk(false);
+	    	} else {
+		    	var rec = { 
+					source: req.params[1], 
+					postdata:JSON.stringify(req.body),
+					cache: new Buffer(body).toString('base64'), 
+					tm: new Date(), 
+					content_type:response.headers['content-type']};
+				db.cache.insert(rec, function (err) {
+					cbk(rec);
+				  });
+	    	}
+	    });
+     };	
+
+	CP.serial(
+		_f,
+		function(data) {
+	    	var rec = (data.results.S1)?data.results.S1:data.results.S2;
+	    	if (rec !== false) {
+		    	res.writeHead(200, {'Content-Type': rec.content_type});
+		    	res.write(new Buffer(rec.cache, 'base64'));
+		    	res.end();	    		
+	    	}
+
+		},
+		3000
+	);
 });
 
 
