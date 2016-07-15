d109c9f0a2c1605dc6eed5a2c0a2379ee40cf116
b2blosangeles <b2blosangeles@gmail.com>
2016-07-15 07:50:15 -0700

    test

--- qalet_cache2.js
+++ qalet_cache2.js
@@ -11,7 +11,7 @@ expireTime	= 604800000;
 	      //  url: 'http://pingiping.com/micro_service/pageHub/',
 			url: 'http://cache2.qalet.com:8880/cache36000/http://pingiping.com/micro_service/pageHub/',
 	        method:  'POST',
-			data: JSON.stringify({age: 32, gender: "F", country: "CHINA"}),
+			data: {age: 32, gender: "F", country: "CHINA"},
 	        encoding: null
 	    }
 		console.log(options);
