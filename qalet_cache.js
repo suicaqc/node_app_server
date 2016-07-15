83178a66324fc2a57366c6e9565e4839a0515f63
b2blosangeles <b2blosangeles@gmail.com>
2016-07-15 08:40:02 -0700

    test

--- qalet_cache.js
+++ qalet_cache.js
@@ -21,6 +21,13 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
+app.all('*', function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "X-Requested-With");
+       res.header('Access-Control-Allow-Headers', 'Content-Type');
+       next();
+});
+
 app.post(/cache(|[0-9]+)\/(\S+)$/i, function(req, res) {
 	var CP = new crowdProcess();
 	var _f = {};
