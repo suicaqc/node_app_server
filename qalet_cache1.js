62ee248df9a5de3ee547c1c8bc302ac2942c9a6d
b2blosangeles <b2blosangeles@gmail.com>
2016-07-18 08:21:18 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -36,67 +36,18 @@ app.post(/cache(|[0-9]+)\/(\S+)$/i, function(req, res) {
 	var postCache  = require(__dirname + '/modules/cacheModule/cacheModule.js');
 	var pc = new postCache(pkg, req, res);
 	pc.post();	
-	return false;
+	return true;
 
 });
 
 
 app.get(/cache(|[0-9]+)\/(\S+)$/i, function (req, res) {
-	var CP = new crowdProcess();
-	var _f = {};
-	var _cachetime = 1000 * ((req.params[0])?req.params[0]:3600);
+	delete require.cache[__dirname + '/modules/cacheModule/cacheModule.js'];
+	var getCache  = require(__dirname + '/modules/cacheModule/cacheModule.js');
+	var gc = new getCache(pkg, req, res);
+	gc.get();	
+	return true;
 
-	_f['S1'] = function(cbk) {
-		db.cache.find({ source: req.params[1] }, function (err, docs) {
-	    	if ((docs[0]) && (new Date() - docs[0].tm < _cachetime)) {
-	    		CP.exit = true;
-				cbk(docs[0]);
-	    	} else {	    		
-	    		db.cache.remove({ source: req.params[1] }, function (err, docs) {
-	    			cbk(false);
-	    		});	
-	    	}
-	    	
-	      });
-     };	
-
-	_f['S2'] = function(cbk) {
-	    var options = {
-	        url: req.params[1],
-	        method:  'GET',
-	        encoding: null
-	    }
-
-	    request(options ,function(error, response, body) {
-	    	if (error) {
-	    		res.send(error.toString());
-	    		cbk(false);
-	    	} else {
-		    	var rec = { 
-					source: req.params[1], 
-					cache: new Buffer(body).toString('base64'), 
-					tm: new Date(), 
-					content_type:response.headers['content-type']};
-				db.cache.insert(rec, function (err) {
-					cbk(rec);
-				  });
-	    	}
-	    });
-     };	
-
-	CP.serial(
-		_f,
-		function(data) {
-	    	var rec = (data.results.S1)?data.results.S1:data.results.S2;
-	    	if (rec !== false) {
-		    	res.writeHead(200, {'Content-Type': rec.content_type});
-		    	res.write(new Buffer(rec.cache, 'base64'));
-		    	res.end();	    		
-	    	}
-
-		},
-		3000
-	);
 });
 
 
