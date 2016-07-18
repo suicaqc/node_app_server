3b42d02ceab2dd2ff549c567b2b4e4a83cae049e
b2blosangeles <b2blosangeles@gmail.com>
2016-07-18 08:19:13 -0700

    test

--- modules/cacheModule/cacheModule.js
+++ modules/cacheModule/cacheModule.js
@@ -86,7 +86,69 @@
 					},
 					3000
 				);
-			}
+			},
+			this.get = function() {
+				var CP = new pkg.crowdProcess();
+				var db 	= {
+					cache 	: new pkg.Nedb({ filename: 'db/cache.db', autoload: true }),
+					auth	: new pkg.Nedb({ filename: 'db/auth.db', autoload: true })
+				}; 
+
+				var _f = {};
+				var _cachetime = 1000 * ((req.params[0])?req.params[0]:3600);
+
+				_f['S1'] = function(cbk) {
+					db.cache.find({ source: req.params[1] }, function (err, docs) {
+						if ((docs[0]) && (new Date() - docs[0].tm < _cachetime)) {
+							CP.exit = true;
+							cbk(docs[0]);
+						} else {	    		
+							db.cache.remove({ source: req.params[1] }, function (err, docs) {
+								cbk(false);
+							});	
+						}
+						
+					  });
+				 };	
+
+				_f['S2'] = function(cbk) {
+					var options = {
+						url: req.params[1],
+						method:  'GET',
+						encoding: null
+					}
+
+					pkg.request(options ,function(error, response, body) {
+						if (error) {
+							res.send(error.toString());
+							cbk(false);
+						} else {
+							var rec = { 
+								source: req.params[1], 
+								cache: new Buffer(body).toString('base64'), 
+								tm: new Date(), 
+								content_type:response.headers['content-type']};
+							db.cache.insert(rec, function (err) {
+								cbk(rec);
+							  });
+						}
+					});
+				 };	
+
+				CP.serial(
+					_f,
+					function(data) {
+						var rec = (data.results.S1)?data.results.S1:data.results.S2;
+						if (rec !== false) {
+							res.writeHead(200, {'Content-Type': rec.content_type});
+							res.write(new Buffer(rec.cache, 'base64'));
+							res.end();	    		
+						}
+
+					},
+					3000
+				);
+			}			
 		};
 
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
