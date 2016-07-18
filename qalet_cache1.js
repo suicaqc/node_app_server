0566b50150498e6227fe38b3fd5d83a391cac875
b2blosangeles <b2blosangeles@gmail.com>
2016-07-18 08:40:07 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -1,17 +1,14 @@
 var 
-express = require('./package/express/node_modules/express'),  
-crowdProcess = require('./package/crowdProcess/crowdProcess'), 
-Nedb = require('./package/nedb/node_modules/nedb'), 
-bodyParser = require('./package/body-parser/node_modules/body-parser'),
-request = require('./package/request/node_modules/request'),
+express = require('./package/express/node_modules/express'),   
+bodyParser = require('./package/body-parser/node_modules/body-parser'),,
 app			= express(),
 expireTime	= 604800000,
 port 		= 8880;
 
 var pkg = {
-	crowdProcess:crowdProcess,
-	Nedb:Nedb,
-	request:request
+	crowdProcess:require('./package/crowdProcess/crowdProcess'),
+	Nedb:require('./package/nedb/node_modules/nedb'),
+	request:require('./package/request/node_modules/request')
 }
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -27,17 +24,15 @@ app.all('*', function(req, res, next) {
 });
 
 app.post(/cache(|[0-9]+)\/(\S+)$/i, function(req, res) {
-	delete require.cache[__dirname + '/modules/cacheModule/cacheModule.js'];
+//	delete require.cache[__dirname + '/modules/cacheModule/cacheModule.js'];
 	var postCache  = require(__dirname + '/modules/cacheModule/cacheModule.js');
 	var pc = new postCache(pkg, req, res);
 	pc.post();	
 	return true;
 
 });
-
-
 app.get(/cache(|[0-9]+)\/(\S+)$/i, function (req, res) {
-	delete require.cache[__dirname + '/modules/cacheModule/cacheModule.js'];
+//	delete require.cache[__dirname + '/modules/cacheModule/cacheModule.js'];
 	var getCache  = require(__dirname + '/modules/cacheModule/cacheModule.js');
 	var gc = new getCache(pkg, req, res);
 	gc.get();	
@@ -54,7 +49,6 @@ app.get(/_cmd(\/|)$/i, function (req, res) {
 		console.log('------');
 		console.log(code);
 	})	
-	var CP = new crowdProcess();
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('command is not exist.aa');
 	res.end();
