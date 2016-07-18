7ef30a2eeb4310a59fbb8ea8146d44d6598156a5
b2blosangeles <b2blosangeles@gmail.com>
2016-07-17 23:01:25 -0700

    test

--- modules/niceWork/niceWork.js
+++ modules/niceWork/niceWork.js
@@ -2,7 +2,7 @@
 		var obj =  function (req, res) {
 			this.callIn = function() {
 				res.writeHead(500, {'Content-Type': 'text/html'});
-				res.write('Nice work D - ' + new Date());
+				res.write('Nice work DE - ' + new Date());
 				res.end()
 			}
 		};
