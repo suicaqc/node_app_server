a477744743b02864884af9cf1c7b0ef45e248c38
b2blosangeles <b2blosangeles@gmail.com>
2016-07-19 08:44:55 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -60,7 +60,7 @@ app.get(/_git(\/|)$/i, function (req, res) {
 	res.end();
 });
 
-app.get(/_microservice\/(*)$/i, function (req, res) {
+app.get(/_microservice\/([0-9a-z]+)$/i, function (req, res) {
 	delete require.cache[__dirname + '/modules/niceWork/niceWork.js'];
 
 	var niceWork  = require(__dirname + '/modules/niceWork/niceWork.js');
