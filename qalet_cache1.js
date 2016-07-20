996d29a32a25e2e1932a117f0a1be558514ad4f2
b2blosangeles <b2blosangeles@gmail.com>
2016-07-19 20:44:22 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -15,12 +15,12 @@ var pkg = {
 			auth	: new Nedb({ filename: '_db/auth.db', autoload: true })
 		}
 }
-/*
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-*/
+/**/
 app.all('*', function(req, res, next) {
        res.header("Access-Control-Allow-Origin", "*");
        res.header("Access-Control-Allow-Headers", "X-Requested-With");
