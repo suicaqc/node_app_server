a29530ca372ea0310f65a2fa4f95a296f06d6e29
b2blosangeles <b2blosangeles@gmail.com>
2016-07-19 22:50:56 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -49,12 +49,13 @@ app.get(/cache(|[0-9]+)\/(\S+)$/i, function (req, res) {
 app.get(/_git(\/|)$/i, function (req, res) {
 	
 	var exec = require('child_process').exec;
-//	exec('git pull && reboot -f', function(err, out, code) {
 	exec('git pull', function(err, out, code) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write('done git pull');
 		res.end();
-	})	
+		exec('git pull && reboot -f', function(err, out, code) {
+		});	
+	});	
 });
 
 app.get(/_microservice\/([0-9a-z]+)(\/|)$/i, function (req, res) {
