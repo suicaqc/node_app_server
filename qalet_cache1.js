bf255686da47c7709a72d998fd4a595c9a66c83c
b2blosangeles <b2blosangeles@gmail.com>
2016-07-19 22:18:31 -0700

    test

--- qalet_cache1.js
+++ qalet_cache1.js
@@ -17,11 +17,11 @@ var pkg = {
 }
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-/*
+
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: false
 })); 
-*/
+
 app.all('*', function(req, res, next) {
        res.header("Access-Control-Allow-Origin", "*");
        res.header("Access-Control-Allow-Headers", "X-Requested-With");
