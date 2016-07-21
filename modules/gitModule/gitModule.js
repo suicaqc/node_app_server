70964bc9f5aa182197eed489445552be5f8668d2
b2blosangeles <b2blosangeles@gmail.com>
2016-07-20 22:15:17 -0700

    test

--- modules/gitModule/gitModule.js
+++ modules/gitModule/gitModule.js
@@ -77,14 +77,17 @@
 		}		
 		this.root = function(reboot) {
 			var exec = require('child_process').exec;
+			console.log(reboot);
 			exec('git pull ', function(err, out, code) {
 				if 	(reboot) {
-					exec('sudo reboot -f &&', function(err, out, code) {});	
+					exec('sudo reboot -f &&', function(err, out, code) {
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.write(out);
+						res.write('Yes, root repository updated E.' + reboot + '===');
+						res.end();						
+					});	
 				}			
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.write(out);
-				res.write('Yes, root repository updated E.' + reboot + '===');
-				res.end();
+
 							
 			});				
 		}
