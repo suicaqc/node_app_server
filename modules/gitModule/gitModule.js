3dd410d194b3e817c396c348227f67e37f1d2cc8
b2blosangeles <b2blosangeles@gmail.com>
2016-07-20 22:41:37 -0700

    test

--- modules/gitModule/gitModule.js
+++ modules/gitModule/gitModule.js
@@ -84,6 +84,8 @@
 					exec('shutdown -r +1', function(err, out, code) {
 						
 						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.write(err);
+						res.write(code);
 						res.write(out);
 						res.write('Yes, root repository updated AB.');
 						res.end();							
