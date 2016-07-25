9e4a5327e076af6708b26340263392b891e2a274
b2blosangeles <b2blosangeles@gmail.com>
2016-07-24 22:15:45 -0700

    test

--- modules/microRouter/microRouter.js
+++ modules/microRouter/microRouter.js
@@ -15,65 +15,28 @@
 				for (o in cfg) {
 					_f[o] =  function(cbk) {
 						
-						
-						
+						var options = {
+							url: cfg[o],
+							method:  'GET',
+							form: req.body.postData,
+							encoding: null
+						}						
+						pkg.request(options ,function(error, response, body) {
+							if (error) {
+								res.send(error.toString());
+								cbk(false);
+							} else {
+								cbk( body);
+							}
+						});						
 					};
 					
 				}
-			res.send(_f);
-			return false;
-			
-				_f['S2'] = function(cbk) {
-					var options = {
-						url: req.params[1],
-						method:  'POST',
-						form: req.body.postData,
-						encoding: null
-					}
-
-
-					pkg.request(options ,function(error, response, body) {
-						if (error) {
-							res.send(error.toString());
-							cbk(false);
-						} else {
-							var rec = { 
-								source: req.params[1], 
-								postdata:JSON.stringify(req.body.postData),
-								cache: new Buffer(body).toString('base64'), 
-								tm: new Date(), 
-								content_type:response.headers['content-type']};
-							pkg.db.post_cache.insert(rec, function (err) {
-								cbk(rec);
-							  });
-						}
-					});
-				 };	
 
 				CP.serial(
 					_f,
 					function(data) {
-						
-						if (!data.results.S0) {
-							res.writeHead(500, {'Content-Type': 'text/html'});
-							res.write('Data format error');
-							res.end();					
-							
-						} else {
-							var rec = (data.results.S1)?data.results.S1:data.results.S2;
-							if (rec) {
-								res.writeHead(200, {'Content-Type': rec.content_type});
-								res.write(new Buffer(rec.cache, 'base64'));
-								res.end();	    		
-							} else {
-								res.writeHead(500, {'Content-Type': 'text/html'});
-								res.write('No result');
-								res.end();					
-							}				
-						}
-						
-
-
+						res.send(data.results);
 					},
 					3000
 				);
