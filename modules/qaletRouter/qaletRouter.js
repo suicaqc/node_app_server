8441e389da78275632566551899275e30ffe4ff1
b2blosangeles <b2blosangeles@gmail.com>
2016-08-02 06:33:49 -0700

    test

---
+++ modules/qaletRouter/qaletRouter.js
@@ -0,0 +1,16 @@
+(function () { 
+	var obj =  function (pkg, env, req, res) {
+		this.load = function() {
+			res('niu' + req.params[0]);
+		};	
+	};
+	
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	} else {
+		window.qaletRouter = function() {
+			return obj; 
+		}
+	}
+	
+})();
