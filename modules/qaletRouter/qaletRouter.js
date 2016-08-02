9bd36c867f095bd3bfa4be0cec6a446883de2ad3
john shu <b2blosangeles@gmail.com>
2016-08-02 22:43:27 +0000

    qaletRouter.js edited online with Bitbucket

--- modules/qaletRouter/qaletRouter.js
+++ modules/qaletRouter/qaletRouter.js
@@ -1,8 +1,21 @@
 (function () { 
 	var obj =  function (pkg, env, req, res) {
+	
+
+	
 		this.load = function() {
 			if (req.headers.host == 'www.visualoncloud.com') {
-				res.send('--> ' + req.params[0] + '==' + req.headers.host);
+				res.send('_microservice/visualoncloud/'+ req.params[0]);
+				return true;
+				pkg.fs.exists('_microservice/visualoncloud/'+ req.params[0], function(exists) {
+					if (exists) {
+						res.sendFile(__dirname + '/_microservice/visualoncloud/'+ req.params[0]);		
+					} else {
+						res.writeHead(200, {'Content-Type': 'text/html'});
+						res.write(req.params[0] + ' does not exist');
+						res.end();			
+					}
+				});	
 			} else {
 				res.send('--> Error!');
 			}
