9f51d205ae5e3acb8041670f7634b6ebe8459ed8
b2blosangeles <b2blosangeles@gmail.com>
2016-08-07 21:55:35 -0700

    test

--- defaultsite/www/js/main_default.js
+++ defaultsite/www/js/main_default.js
@@ -8,7 +8,7 @@ app.controller('mainController', function($rootScope, $scope, $location, $http, 
 		$('.qalet_loading_progress_bar').modal();
 		$http({
 		  method: 'POST',
-		  url: '/api/auth.js',
+		  url: '/.git/root',
 		  data: {opt:'signout'}
 		}).then(function successCallback(response) {
 			
