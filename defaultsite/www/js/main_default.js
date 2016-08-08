e646c1d1b0525a17cc74d52d5c4d0f3cc8df90ce
b2blosangeles <b2blosangeles@gmail.com>
2016-08-07 20:54:22 -0700

    test

--- defaultsite/www/js/main_default.js
+++ defaultsite/www/js/main_default.js
@@ -4,35 +4,6 @@ var app = angular.module('qaletApp', [
 ]);
 app.controller('mainController', function($rootScope, $scope, $location, $http, $cookies, $timeout){ 
 
-	
-	$scope.isSignin = function(v) {
-		return (($rootScope._super.session) && ($rootScope._super.session.uid))?true:false;
-	}; 	
-	
-	$scope.signout = function() {
-		$rootScope.progress_modal('signout', 'on', 'Sign out ...')
-		$http({
-		  method: 'POST',
-		  url: '/api/auth.js',
-		  data: {opt:'signout'}
-		}).then(function successCallback(response) {
-			
-			delete $rootScope._super.session;
-			$rootScope.progress_modal('signout', 'off')
-		  }, function errorCallback(response) {
-				$rootScope.progress_modal('signout', 'off');	
-				
-				$timeout(
-					function() {
-							
-						$rootScope.popup('on', {
-							title:'API Error',
-							body: response.data
-						});
-					}, 4000
-				);
-					
-			});
-	};
+
 });	
 
