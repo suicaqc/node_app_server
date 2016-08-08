c3339fa0bac1217495dcc45b143d4215c6bb0e59
b2blosangeles <b2blosangeles@gmail.com>
2016-08-07 21:30:50 -0700

    test

--- defaultsite/www/js/main_default.js
+++ defaultsite/www/js/main_default.js
@@ -15,7 +15,7 @@ app.controller('mainController', function($rootScope, $scope, $location, $http, 
 	$scope.listServices = function() {
 		$scope.popup('on', {
 			title:'Services',
-			body: $sce.trustAsHtml('Under construction<br/><img src="/images/Under-Construction.gif">')
+			body: $sce.trustAsHtml('Under construction<br/><img src="/images/Under-Construction.gif">&hellip;')
 		});			
 		
 	}
@@ -23,7 +23,7 @@ app.controller('mainController', function($rootScope, $scope, $location, $http, 
 	$scope.report = function() {
 		$scope.popup('on', {
 			title:'Report',
-			body: $sce.trustAsHtml('Under construction<br/><img src="/images/Under-Construction.gif">')
+			body: $sce.trustAsHtml('Under construction<br/><img src="/images/Under-Construction.gif">&hellip;')
 		});			
 		
 	}
