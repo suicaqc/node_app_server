93863118626e57be8700deacac430f9d94e64904
b2blosangeles <b2blosangeles@gmail.com>
2016-08-10 18:31:59 -0700

    test

--- modules/qaletRouter/qaletRouter.js
+++ modules/qaletRouter/qaletRouter.js
@@ -104,7 +104,7 @@ app.get(/_git\/(|[0-9a-z]+)$/i, function (req, res) {
 */			
 			var gitP = req.params[0].match(/_git\/(|.+)$/i);
 			if (gitP) {
-				//res.send(gitP);
+				res.send(gitP);
 			//	res.send(env.root_path);
 				delete require.cache[env.root_path + '/modules/gitModule/gitModule.js'];
 				var gitModule  = require(env.root_path + '/modules/gitModule/gitModule.js');
